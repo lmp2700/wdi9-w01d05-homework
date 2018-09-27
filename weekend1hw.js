a4028fca3b220c4e515a412b83cba9fe77dfa152
Liz Steak <lizmaepark@gmail.com>
2018-09-27 13:42:53 -0600

    Arrays & Control flow

--- weekend1hw.js
+++ weekend1hw.js
@@ -81,69 +81,66 @@
 // Write a loop that will print out all the numbers from 0 to 10, inclusive.
 // Write a loop that will print out all the numbers from 10 up to and including 400.
 // Write a loop that will print out every third number starting with 12 and going no higher than 4000.
-for(let i = 0; i <= 10; i++) {
-    console.log(i)
-}
+// for(let i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
 
-for(let i = 10; i <= 400; i++) {
-    console.log(i)
-}
+// for(let i = 10; i <= 400; i++) {
+//     console.log(i)
+// }
 
-for(let i = 12; i <= 4000; i += 3) {
-    console.log(i)
-}
-// 🔴 Commit.
+// for(let i = 12; i <= 4000; i += 3) {
+//     console.log(i)
+// }
 
-// B. Get even
+// --> B. Get even
 // Print out the even numbers that are within the range of 1 - 100.
-
 // Adjust your code to add a message next to even numbers only that says: "<-- is an even number".
-
+// NOT PRINTING EVEN NUMBERS; PRINTING ALL NUMBERS
+// const i = 1
+// for(let i = 1; i <= 100; i++) {
+//     if(i % 2 === 0);
+//     console.log(i + ' is an even number')
+// }
 // 🔴 Commit.
 
-// C. Give me Five
+// --> C. Give me Five
+// PRINTING ALL NUMBERS AGAIN
 // For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five.
-// Example Output:
-
-// I found a 5. High five!
-// I found a 10. High five!
+// for(let i = 0; i <= 100; i++) {
+//     if(i % 5 === 0);
+//     console.log('I found a ' + i + ' I found a number. High five!');
+//     if(i % 3 === 0);
+//     console.log('I found a number. Three is a crowd.')
+// }
 // Add to the code from above to print out "I found a number. Three is a crowd" if the number is a multiple of three.
-// Example Output:
-
-// I found a 3. Three is a crowd
-// I found a 5. High five!
-// I found a 6. Three is a crowd
-// I found a 9. Three is a crowd
-// I found a 10. High five!
 // 🔴 Commit.
 
 // D. Savings account
 // Write code that will save the sum of all the numbers between 1 - 10 to a variable called bank_account.
 // Check your work! Your banck_account should have $55 in it.
-
 // You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
 // Check your work! Your banck_account should have $10,100 in it.
-
 // 🔴 Commit.
 
 // E. Multiples of 3 and 5
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
-// Find the sum of all the multiples of 3 or 5 below 1000. If a previous question you've done has helpful bits of code in it that partially solves this problem, look back at them.
-
+// Find the sum of all the multiples of 3 or 5 below 1000. If a previous question you've done has helpful bits of code in it that partially 
+// solves this problem, look back at them.
 // You just solved Project Euler problem 1!
-
-// Are you having dejà vu? This just in! From the "Read the entire problem before you start" dept: This problem was on a previous assignment. You may skip it if you've already done it, just include a comment saying that you've already done it. If you've now done the problem twice, perhaps next time you'll read the whole problem before starting it.
+// Are you having dejà vu? This just in! From the "Read the entire problem before you start" dept: This problem was on a previous assignment. 
+// You may skip it if you've already done it, just include a comment saying that you've already done it. If you've now done the problem 
+// twice, perhaps next time you'll read the whole problem before starting it.
 
 // 🔴 Commit.
 
 // III. Arrays & Control flow
 // A. Talk about it:
-// What are the things in an array called?
+What are the things in an array called? Elements
 
-// Do Arrays guarantee those things will be in order?
+Do Arrays guarantee those things will be in order? Arrays guarantee the order that you put the elements in
 
-// What real-life thing could you model with an array?
+What real-life thing could you model with an array? Breeds of cats
 
 // 🔴 Commit.
 
